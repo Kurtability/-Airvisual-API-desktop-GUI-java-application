b9ee2b69ae832e7de8ebe19e52bc73701bd48433
Kurt Lu <falu5003@uni.sydney.edu.au>
2021-06-07 00:51:54 +1000

    jsonTest1 green

--- src/main/java/presenter/JsonParser.java
+++ src/main/java/presenter/JsonParser.java
@@ -10,7 +10,27 @@ import java.util.LinkedHashMap;
 public class JsonParser {
     //https://api.airvisual.com/v2/countries?key=f942928c-b2ec-4ab7-a7d9-009d88f63d36
     public static ArrayList<String> parseSupportedCountries(String toBeParsed){
-        return null;
+        ArrayList<String> countries = new ArrayList<>();
+
+
+        JSONObject obj = new JSONObject(toBeParsed);
+
+        if(obj.has("error")){
+            JSONObject error = obj.getJSONObject("error");
+            String error_message = error.getString("message");
+            System.out.println(error_message);
+        }
+
+        String status = obj.getString("status");
+        System.out.println("status: "+ status);
+
+        JSONArray arr = obj.getJSONArray("data");
+        for (int i = 0; i < arr.length(); i++) {
+            countries.add(arr.getJSONObject(i).getString("country"));
+            //String country = arr.getJSONObject(i).getString("country");
+            //System.out.println("country: " + country);
+        }
+        return countries;
     }
 
 
